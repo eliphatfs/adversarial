07b2c00ceeba71568419f62c6e9928f74b801e77
eliphat.f.s <shiruoxi2010@163.com>
2021-03-30 08:25:08 +0800

    Make `load` device compatible

--- adv/model.py
+++ adv/model.py
@@ -19,26 +19,32 @@ def filter_state_dict(state_dict):
     return new_state_dict
 
 
+def load_w(model, path):
+    pref = next(model.parameters())
+    model.load_state_dict(torch.load(path, map_location=pref.device))
+
+
 def get_model_for_attack(model_name):
     if model_name == 'model1':
         model = ResNet34()
-        model.load_state_dict(torch.load("models/weights/resnet34.pt"))
+        load_w(model, "models/weights/resnet34.pt")
     elif model_name == 'model2':
         model = ResNet18()
-        model.load_state_dict(torch.load('models/weights/resnet18_AT.pt'))
+        load_w(model, "models/weights/resnet18_AT.pt")
     elif model_name == 'model3':
         model = SmallResNet()
-        model.load_state_dict(torch.load('models/weights/res_small.pth'))
+        load_w(model, "models/weights/res_small.pth")
     elif model_name == 'model4':
         model = WideResNet34()
-        model.load_state_dict(filter_state_dict(
-            torch.load('models/weights/trades_wide_resnet.pt')))
+        load_w(model, "models/weights/trades_wide_resnet.pt")
     elif model_name == 'model5':
         model = WideResNet()
-        model.load_state_dict(torch.load(
-            'models/weights/wideres34-10-pgdHE.pt'))
+        load_w(model, "models/weights/wideres34-10-pgdHE.pt")
     elif model_name == 'model6':
         model = WideResNet28()
+        pref = next(model.parameters())
         model.load_state_dict(filter_state_dict(torch.load(
-            'models/weights/RST-AWP_cifar10_linf_wrn28-10.pt')))
+            'models/weights/RST-AWP_cifar10_linf_wrn28-10.pt',
+            map_location=pref.device
+        )))
     return model
