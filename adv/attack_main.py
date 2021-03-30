0799ebb77d6acee614ba0e5beea0573386c34afb
eliphat.f.s <shiruoxi2010@163.com>
2021-03-30 08:25:08 +0800

    Fixed more Warnings

--- adv/attack_main.py
+++ adv/attack_main.py
@@ -16,12 +16,14 @@ def parse_args():
     parser.add_argument('--step_size', type=int, default=0.003,
                         help='step size for pgd attack(default:0.003)')
     parser.add_argument('--epsilon', type=float, default=8/255.0,
-                        help='max distance for pgd attack (default epsilon=8/255)')
+                        help='max distance for pgd attack (default: 8/255)')
     parser.add_argument('--perturb_steps', type=int, default=20,
                         help='iterations for pgd attack (default pgd20)')
     parser.add_argument('--model_name', type=str, default="")
-    parser.add_argument('--model_path', type=str,
-                        default="./models/weights/model-wideres-pgdHE-wide10.pt")
+    parser.add_argument(
+        '--model_path', type=str,
+        default="./models/weights/model-wideres-pgdHE-wide10.pt"
+    )
     parser.add_argument('--gpu_id', type=str, default="0,1")
     return parser.parse_args()
 
@@ -31,17 +33,15 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id  # 多卡机设置使用的gpu卡号
     gpu_num = max(len(args.gpu_id.split(',')), 1)
     device = torch.device('cpu')
+    # model = nn.DataParallel(model, device_ids=[i for i in range(gpu_num)])
     if args.model_name != "":
-        model = get_model_for_attack(args.model_name).to(
-            device)   # 根据model_name, 切换要攻击的model
-        # model = nn.DataParallel(model, device_ids=[i for i in range(gpu_num)])
-
+        model = get_model_for_attack(args.model_name).to(device)
+        # 根据model_name, 切换要攻击的model
     else:
         # 防御任务, Change to your model here
         model = WideResNet()
         model.load_state_dict(torch.load(
             'models/weights/wideres34-10-pgdHE.pt'))
-        # model = nn.DataParallel(model, device_ids=[i for i in range(gpu_num)])
     # 攻击任务：Change to your attack function here
     # Here is a attack baseline: PGD attack
     attack = PGDAttack(args.step_size, args.epsilon, args.perturb_steps)
@@ -50,4 +50,6 @@ if __name__ == '__main__':
     natural_acc, robust_acc, distance = eval_model_with_attack(
         model, test_loader, attack, device)
     print(
-        f"Natural Acc: {natural_acc:.5f}, Robust acc: {robust_acc:.5f}, distance:{distance:.5f}")
+        "Natural Acc: %.5f, Robust acc: %.5f, distance: %.5f" %
+        (natural_acc, robust_acc, distance)
+    )
