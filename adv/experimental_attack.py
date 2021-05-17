13ddd72fe7e84f8b1d2df21f69962bcea3e7d3db
eliphat.f.s <shiruoxi2010@163.com>
2021-05-17 22:20:44 +0800

    Not so bad krylov

--- adv/experimental_attack.py
+++ adv/experimental_attack.py
@@ -48,9 +48,45 @@ class ExpAttack():
                 if pred[i] != y_cpu[i]:
                     results[i] = x_adv[i]
         return results
-
+    
     def __call__(self, model, x, y):
-        return self.frank_wolfe(model, x, y)
+        model.eval()
+
+        def func(cx):
+            logits = model(cx)
+            return torch.nn.functional.cross_entropy(logits, y)
+
+        def safe_jac(cx):
+            jac = torch.autograd.functional.jacobian(func, cx)
+            jac = torch.randn_like(jac) * 1e-7 + jac
+            return jac
+
+        tg = x.flatten(1).shape[-1] ** 0.5
+        b = safe_jac(x)
+        krylov = [torch.sign(b) / tg]
+        batch_dot = lambda u, v: torch.bmm(u.flatten(1).unsqueeze(-2), v.flatten(1).unsqueeze(-1)).flatten(0)
+        H = torch.zeros(
+            [x.shape[0], self.perturb_steps + 1, self.perturb_steps],
+            dtype=x.dtype,
+            device=x.device
+        )
+        last = krylov[-1]
+        for i in range(self.perturb_steps):
+            v = safe_jac(x + last * self.epsilon * 0.7) - b
+            for j, u in enumerate(krylov):
+                H[:, j, i] = batch_dot(v, u)
+                v = v - H[:, j, i].reshape(-1, 1, 1, 1) * u
+            H[:, i + 1, i] = torch.linalg.norm(v.flatten(1), dim=-1)
+            v = v / H[:, i + 1, i].reshape(-1, 1, 1, 1)
+            krylov.append(v)
+            last = v
+        U = torch.stack(krylov[:-1])
+        Uf = U.flatten(2).permute(1, 2, 0)  # n, *, m
+        # x_adv = (x + torch.sign(b) * self.epsilon).detach().clone()
+        x_adv = x.detach().clone()
+
+
+    def not_so_bad_krylov(self, model, x, y):
         model.eval()
 
         def func(cx):
@@ -61,40 +97,68 @@ class ExpAttack():
         def safe_jac(cx):
             jac = torch.autograd.functional.jacobian(func, cx)
             jac = torch.randn_like(jac) * 1e-7 + jac
-            return torch.sign(jac)
+            return jac
 
-        krylov = [safe_jac(x)]
-        b = krylov[0]
+        tg = x.flatten(1).shape[-1] ** 0.5
+        b = safe_jac(x)
+        krylov = [b / torch.norm(b.flatten(1), -1).reshape(-1, 1, 1, 1)]
+        batch_dot = lambda u, v: torch.bmm(u.flatten(1).unsqueeze(-2), v.flatten(1).unsqueeze(-1)).flatten(0)
+        H = torch.zeros(
+            [x.shape[0], self.perturb_steps + 1, self.perturb_steps],
+            dtype=x.dtype,
+            device=x.device
+        )
+        last = krylov[-1]
         for i in range(self.perturb_steps):
-            krylov.append(safe_jac(x + krylov[i] * self.epsilon))
+            v = safe_jac(x + last * self.epsilon * tg) - b
+            for j, u in enumerate(krylov):
+                H[:, j, i] = batch_dot(v, u)
+                v = v - H[:, j, i].reshape(-1, 1, 1, 1) * u
+            H[:, i + 1, i] = torch.linalg.norm(v.flatten(1), dim=-1)
+            v = v / H[:, i + 1, i].reshape(-1, 1, 1, 1)
+            krylov.append(v)
+            last = v
         U = torch.stack(krylov[:-1])
-        AU = torch.stack(krylov[1:])
+        # AU = torch.stack(krylov[1:])
+        # x_adv = (x + torch.sign(b) * self.epsilon).detach().clone()
         x_adv = x.detach().clone()
-        delta = b * self.epsilon
-        lam = torch.norm(b.flatten(1), 2, dim=-1) / b.flatten(1).shape[-1] ** 0.5
-        for _ in range(4):
-            A = (AU.flatten(2) - lam.reshape(1, -1, 1) * U.flatten(2)).permute(1, 2, 0)  # [n, *, m]
-            B = -b.flatten(1).unsqueeze(-1)  # [n, *, 1]
+        # delta = b * self.epsilon
+        # .reshape(1, -1, 1)
+
+        def resolve(lam):
+            # A = (AU.flatten(2) - lam.reshape(1, -1, 1) * U.flatten(2)).permute(1, 2, 0)  # [n, *, m]
             # AtA k = At B
-            k, _ = torch.solve(A.transpose(2, 1).bmm(B), A.transpose(2, 1).bmm(A))
+            # k, _ = torch.solve(A.transpose(2, 1).bmm(B), A.transpose(2, 1).bmm(A))
             Uf = U.flatten(2).permute(1, 2, 0)  # [n, *, m]
+            B = b.flatten(1).unsqueeze(-1)  # [n, *, 1]
+            # (H - lam I) k = -Ut B
+            k, _ = torch.solve(
+                -Uf.transpose(2, 1).bmm(B),
+                H[:, :-1, :] - lam * Uf.transpose(2, 1).bmm(Uf)
+            )
             delta = Uf.bmm(k).reshape(*x.shape)
-            lam = torch.norm(AU.flatten(2).permute(1, 2, 0).bmm(k).squeeze(-1), 2, dim=-1) / b.flatten(1).shape[-1] ** 0.5
+            return delta
+
+        # lam = torch.norm(b.flatten(1), 2, dim=-1) / b.flatten(1).shape[-1] ** 0.5
+        # print(lam)
+        lam = 5
+        active_indices = list(range(x.shape[0]))
+        removed = set()
+        for _ in range(19):
+            delta = resolve(lam)
+            fooled = 0
             with torch.no_grad():
-                pertubed_sample = torch.min(
-                    torch.max(x + delta, x - self.epsilon), x + self.epsilon
-                )
-                pertubed_sample = torch.clamp(pertubed_sample, 0.0, 1.0)
-                y_pred = model(pertubed_sample).argmax(-1)
-                for i, (lab_pred, lab_true) in enumerate(zip(y_pred, y)):
+                perturbed_sample = x + torch.sign(delta) * self.epsilon
+                perturbed_sample = torch.clamp(perturbed_sample, 0.0, 1.0)
+                y_pred = model(perturbed_sample[active_indices]).argmax(-1)
+                for i, lab_pred, lab_true in zip(active_indices, y_pred, y[active_indices]):
                     if lab_pred != lab_true:
-                        x_adv[i] = pertubed_sample[i]
-                pertubed_sample = x + torch.sign(delta) * self.epsilon
-                pertubed_sample = torch.clamp(pertubed_sample, 0.0, 1.0)
-                y_pred = model(pertubed_sample).argmax(-1)
-                for i, (lab_pred, lab_true) in enumerate(zip(y_pred, y)):
-                    if lab_pred != lab_true:
-                        x_adv[i] = pertubed_sample[i]
+                        x_adv[i] = perturbed_sample[i]
+                        removed.add(i)
+                        fooled += 1
+                active_indices = [i for i in active_indices if i not in removed]
+            # print(lam, fooled)
+            lam *= 3 / 4
         return x_adv
 
 
