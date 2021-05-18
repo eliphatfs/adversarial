c5134a7d66dd70d440687eb55f6e49a5117dbb04
eliphat.f.s <shiruoxi2010@163.com>
2021-05-18 23:12:21 +0800

    Optimized Sobol sampling

--- adv/sobol_happy_attack.py
+++ adv/sobol_happy_attack.py
@@ -13,15 +13,17 @@ class SobolHappyAttack():
     def __call__(self, model, x, y):
         model.eval()
         x_adv = x.detach()
-        succeeded_attacks = x.detach()
-        sobol_eng = torch.quasirandom.SobolEngine(x_adv.flatten(0).shape[0])
+        succeeded_attacks = x.detach().clone()
+        sobol_eng = torch.quasirandom.SobolEngine(x_adv.flatten(2).shape[-1])
         for i in range(self.perturb_steps):
-            pertub = torch.sign(sobol_eng.draw(1).to(x_adv.device)-0.5).reshape_as(x_adv)
+            pertub = torch.sign(sobol_eng.draw(3).to(x_adv.device)-0.5).reshape(*x_adv.shape[1:])
             x_adv = x.detach() + pertub.detach()
             x_adv = torch.min(
                 torch.max(x_adv, x - self.epsilon), x + self.epsilon)
             x_adv = torch.clamp(x_adv, 0.0, 1.0)
-            for idx, (img, label) in enumerate(zip(x_adv, y)):
-                if model(img.unsqueeze(0)).max(-1)[-1].item() != label.item():
-                    succeeded_attacks[idx] = img
+            with torch.no_grad():
+                y_pred = model(x_adv).argmax(-1)
+            for i, (lab_pred, lab_true) in enumerate(zip(y_pred, y)):
+                if lab_pred != lab_true:
+                    succeeded_attacks[i] = x_adv[i]
         return succeeded_attacks
