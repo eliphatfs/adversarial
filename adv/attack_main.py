48f2e99275c11ee0d1aa3b3dc4b1f38dacd9068f
eliphat.f.s <shiruoxi2010@163.com>
2021-05-22 10:31:30 +0800

    Ensembling

--- adv/attack_main.py
+++ adv/attack_main.py
@@ -26,7 +26,7 @@ import vgg
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test Robust Accuracy')
-    parser.add_argument('--batch-size', type=int, default=4, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
                         help='input batch size for training (default: 128)')
     parser.add_argument('--step_size', type=int, default=0.003,
                         help='step size for pgd attack(default:0.003)')
@@ -34,12 +34,12 @@ def parse_args():
                         help='max distance for pgd attack (default: 8/255)')
     parser.add_argument('--perturb_steps', type=int, default=20,
                         help='iterations for pgd attack (default pgd20)')
-    parser.add_argument('--model_name', type=str, default="model6")
+    parser.add_argument('--model_name', type=str, default="")
     parser.add_argument(
         '--model_path', type=str,
         default="./models/weights/model-wideres-pgdHE-wide10.pt"
     )
-    parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument('--device', type=str, default="cpu")
     return parser.parse_args()
 
 
@@ -55,6 +55,27 @@ def parse_args():
         fo.write(code)'''
 
 
+def merge_state_dicts(dicts):
+    refd = dicts[0]
+    result = dict()
+    if isinstance(refd, dict):
+        for k in refd.keys():
+            result[k] = merge_state_dicts([d[k] for d in dicts])
+        return result
+    else:
+        return sum(dicts)
+
+
+def divide_state_dict(d, n):
+    result = dict()
+    if isinstance(d, dict):
+        for k in d.keys():
+            result[k] = divide_state_dict(d[k], n)
+        return result
+    else:
+        return d / n
+
+
 if __name__ == '__main__':
     args = parse_args()
     device = torch.device(args.device)
@@ -64,11 +85,18 @@ if __name__ == '__main__':
     else:
         # 防御任务, Change to your model here
         model = vgg.vgg13_bn().to(device)
-        model.load_state_dict(torch.load('vgg13bn_regm.dat', map_location=torch.device(device)))
+        ensemble = torch.load('vgg13bn_regm1.dat', map_location=torch.device(device))
+        '''for i in range(1, 7):
+            ensemble = merge_state_dicts([
+                ensemble,
+                torch.load('vgg13bn_regm%d.dat' % i, map_location=torch.device(device))
+            ])
+        ensemble = divide_state_dict(ensemble, 7)'''
+        model.load_state_dict(ensemble)
     # 攻击任务：Change to your attack function here
     # Here is a attack baseline: PGD attack
-    model = nn.DataParallel(model, device_ids=[0, 4, 6, 7])
-    attack = ExpAttack(args.step_size, args.epsilon, args.perturb_steps)
+    # model = nn.DataParallel(model, device_ids=[0, 4, 6, 7])
+    attack = PGDAttack(args.step_size, args.epsilon, args.perturb_steps)
     model.eval()
     test_loader = get_test_cifar(args.batch_size)
     natural_acc, robust_acc, distance = eval_model_with_attack(
