11c5db62875a077071eae93c72a795c3410fd3e8
YBRua <YBiuR0929@163.com>
2021-06-06 15:30:11 +0800

    Updated Report and .gitignore

--- adv/attack_main.py
+++ adv/attack_main.py
@@ -1,9 +1,13 @@
 # %%
 
+from models.resnet import ResNet18, ResNet34
+from models.preactresnet import PreActResNet18, PreActResNet34
+from models import WideResNet28
 from adamp_stats import attack_and_analyze_stats
 import torch
 import torch.nn as nn
 import pickle
+import sys
 
 from utils import get_test_cifar
 from pgd_attack import PGDAttack
@@ -36,12 +40,13 @@ def parse_args():
                         help='max distance for pgd attack (default: 8/255)')
     parser.add_argument('--perturb_steps', type=int, default=20,
                         help='iterations for pgd attack (default pgd20)')
-    parser.add_argument('--model_name', type=str, default="model6")
+    parser.add_argument('--model_name', type=str, default="")
     parser.add_argument(
         '--model_path', type=str,
         default="./models/weights/model-wideres-pgdHE-wide10.pt"
     )
     parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument('--attacker', choices=['pgd', 'fw'], default='pgd')
     return parser.parse_args()
 
 
@@ -86,19 +91,18 @@ if __name__ == '__main__':
         # 根据model_name, 切换要攻击的model
     else:
         # 防御任务, Change to your model here
-        model = vgg.vgg13_bn().to(device)
-        ensemble = torch.load('vgg13bn_regm1.dat', map_location=torch.device(device))
-        '''for i in range(1, 7):
-            ensemble = merge_state_dicts([
-                ensemble,
-                torch.load('vgg13bn_regm%d.dat' % i, map_location=torch.device(device))
-            ])
-        ensemble = divide_state_dict(ensemble, 7)'''
-        model.load_state_dict(ensemble)
+        model = WideResNet28().to(device)
+        checkpoint = torch.load('./models/weights/WideResNet28TRADE_FWAWP-best.pt')
+        model.load_state_dict(checkpoint['model'])
     # 攻击任务：Change to your attack function here
     # Here is a attack baseline: PGD attack
     # model = nn.DataParallel(model, device_ids=[0, 4, 6, 7])
-    attack = ExpAttack(args.step_size, args.epsilon, args.perturb_steps)
+    if args.attacker == 'fw':
+        print('Using FW', file=sys.stderr)
+        attack = ExpAttack(args.step_size, args.epsilon, args.perturb_steps)
+    else:
+        print('Using PGD20', file=sys.stderr)
+        attack = PGDAttack(args.step_size, args.epsilon, args.perturb_steps)
     model.eval()
     test_loader = get_test_cifar(args.batch_size)
     # natural_acc, robust_acc, distance = eval_model_with_attack(
