bfe23b58a78711eeb4721852afeefd16e70ef4f2
eliphatfs <flandre@scarletx.cn>
2021-09-04 19:56:47 +0800

    Many strange training updates

--- adv/strange_training.py
+++ adv/strange_training.py
@@ -8,7 +8,7 @@ import math
 from attack.energy_attack import EnergyAttack
 
 
-def patches_zero_mean(bchw, kernel=5, stride=1, pad_same=True):
+def patches_of(bchw, kernel=5, stride=1, pad_same=True, zero_mean=True):
     b, c, h, w = bchw.shape
     if pad_same:
         h2 = math.ceil(h / stride)
@@ -16,11 +16,16 @@ def patches_zero_mean(bchw, kernel=5, stride=1, pad_same=True):
         pad_h = (h2 - 1) * stride + (kernel - 1) + 1 - h
         pad_w = (w2 - 1) * stride + (kernel - 1) + 1 - w
         x = F.pad(bchw, (pad_h//2, pad_h - pad_h//2, pad_w//2, pad_w - pad_w//2))
+    else:
+        x = bchw
     patches = x.unfold(2, kernel, stride).unfold(3, kernel, stride)
     patches = patches.permute(0, 4, 5, 1, 2, 3).contiguous()
     
     patches = patches.view(b, -1, patches.shape[-2], patches.shape[-1])
-    return patches - patches.mean([-1, -2, -3], keepdim=True)
+    if zero_mean:
+        return patches - patches.mean([-1, -2, -3], keepdim=True)
+    else:
+        return patches
 
 
 class MinMaxPool(nn.Module):
@@ -30,7 +35,7 @@ class MinMaxPool(nn.Module):
         self.mp2 = maxpool_gen()
 
     def forward(self, x):
-        return torch.cat([self.mp1(x), -self.mp2(-x)], 1)
+        return torch.cat([self.mp1(x), self.mp2(-x)], 1)
 
 
 class FixedPCA(nn.Module):
@@ -40,17 +45,80 @@ class FixedPCA(nn.Module):
         self.kernel = kernel
 
     def forward(self, x):
-        patch = patches_zero_mean(x, self.kernel)
-        return torch.einsum("bihw,ij->bjhw", patch, self.coef)
+        patch = patches_of(x, self.kernel)
+        r = torch.einsum("bihw,ij->bjhw", patch, self.coef)
+        return torch.cat([r, -r], 1)
 
 
 def next_pca_weights(image_loader, tn_repr, kernel, in_feat, out_feat):
     cov = torch.zeros([kernel ** 2 * in_feat] * 2).to(tn_repr)
     for x in image_loader:
-        patch = patches_zero_mean(x.to(tn_repr), kernel)
+        patch = patches_of(x.to(tn_repr), kernel, pad_same=False)
         cov = cov + torch.einsum('bphw,bqhw->pq', patch, patch)
     sigma, eigv = torch.linalg.eigh(cov)
     return eigv[:, -out_feat:]
+
+
+def next_lda_weights(image_loader, tn_repr, kernel, in_feat, out_feat):
+    mu = torch.zeros([kernel ** 2 * in_feat]).to(tn_repr)
+    w = 0
+    for x in image_loader:
+        patch = patches_of(x.to(tn_repr), kernel, zero_mean=False)
+        mu = mu + patch.mean([-1, -2]).sum(0)
+        w += len(patch)
+    sw = torch.zeros([kernel ** 2 * in_feat] * 2).to(tn_repr)
+    st = torch.zeros([kernel ** 2 * in_feat] * 2).to(tn_repr)
+    mu = mu / w
+    for x in image_loader:
+        patch = patches_of(x.to(tn_repr), kernel, zero_mean=False)
+        internal = patch - patch.mean([-1, -2], keepdim=True)
+        total = patch - mu.view(-1, 1, 1)
+        sw = sw + torch.einsum("bphw,bqhw->pq", internal, internal)
+        st = st + torch.einsum("bphw,bqhw->pq", total, total)
+    sj = torch.linalg.solve(sw, st - sw)
+    sigma, eigv = torch.linalg.eigh(sj)
+    return eigv[:, -out_feat:]
+
+
+def next_cpt_weights(image_loader, tn_repr, kernel, in_feat, out_feat):
+    c = 0
+    for x in image_loader:
+        patch = patches_of(x.to(tn_repr), kernel, zero_mean=False)
+        c += patch[:, 0].numel()
+
+    w = torch.randn([c, out_feat]).to(tn_repr).abs() * math.sqrt(0.5 / out_feat)
+    h = torch.randn([in_feat * kernel ** 2, out_feat]).to(tn_repr).abs() * math.sqrt(0.5 / out_feat)
+
+    def update_rule():
+        loss = 0.0
+        rh = torch.zeros_like(h)
+        rw = torch.zeros_like(w)
+        c = 0
+        for x in image_loader:
+            patch = patches_of(x.to(tn_repr), kernel, zero_mean=False)
+            patch = patch.permute(1, 0, 2, 3).flatten(1)  # [f, L]
+            L = patch[0].numel()
+            rh += patch.mm(w[c: c + L])  # [f, L], [L, o] -> [f, o]
+            rw[c: c + L] = patch.t().mm(h)  # [L, f], [f, o] -> [L, o]
+            loss += ((patch.t() - w[c: c + L].mm(h.t())) ** 2).sum()
+            c += L
+        rh *= h
+        rh /= h.mm(w.t().mm(w))
+        rw *= w
+        rw /= w.mm(h.t().mm(h))
+        rh.nan_to_num_(0, 0, 0)
+        rw.nan_to_num_(0, 0, 0)
+        return loss, rw, rh
+
+    while True:
+        loss, rw, rh = update_rule()
+        print(loss)
+        w = rw
+        loss, rw, rh = update_rule()
+        print(loss, (rh - h).abs().sum())
+        if (rh - h).abs().sum() < 0.1:
+            return rh
+        h = rh
 
 
 class WrappedModel(nn.Module):
@@ -64,36 +132,46 @@ class WrappedModel(nn.Module):
         return torch.cat([self.wrap(sb) for sb in subbatches])
 
 
+class SomeActivation(nn.Module):
+    def forward(self, x):
+        return F.relu(x)
+
+
+class InputProcess(nn.Module):
+    def forward(self, x):
+        return (x - 0.5) / 0.5
+
+
 if __name__ == '__main__':
     train, test = prepare_cifar(400, 400)
     tn_repr = torch.zeros([3]).to('cuda:1')
     train_x = [x for x, _ in train]
     encoder_config = [
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 5, 3, 64), 5),
-        lambda _: nn.Tanh(),
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 64, 64), 3),
-        lambda _: nn.Tanh(),
-        lambda _: MinMaxPool(lambda: nn.MaxPool2d(2, 2)),  # 16
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 128, 128), 3),
-        lambda _: nn.Tanh(),
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 128, 128), 3),
-        lambda _: nn.Tanh(),
-        lambda _: MinMaxPool(lambda: nn.MaxPool2d(2, 2)),  # 8
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 256, 256), 3),
-        lambda _: nn.Tanh(),
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 256, 256), 3),
-        lambda _: nn.Tanh(),
-        lambda _: MinMaxPool(lambda: nn.MaxPool2d(2, 2)),  # 4
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 512, 512), 3),
-        lambda _: nn.Tanh(),
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 512, 512), 3),
-        lambda _: nn.Tanh(),
-        lambda _: MinMaxPool(lambda: nn.MaxPool2d(2, 2)),  # 2
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 1024, 512), 3),
-        lambda _: nn.Tanh(),
-        lambda x: FixedPCA(next_pca_weights(x, tn_repr, 3, 512, 512), 3),
-        lambda _: nn.Tanh(),
-        lambda _: MinMaxPool(lambda: nn.AdaptiveMaxPool2d((1, 1))),  # 1
+        lambda x: FixedPCA(next_cpt_weights(x, tn_repr, 5, 3, 32), 5),
+        lambda _: nn.BatchNorm2d(64, affine=False, track_running_stats=False),
+        lambda _: SomeActivation(),
+        lambda _: nn.MaxPool2d(2, 2),  # 16
+        # lambda x: FixedPCA(next_cpt_weights(x, tn_repr, 3, 64, 64), 3),
+        # lambda _: nn.BatchNorm2d(128, affine=False, track_running_stats=False),
+        # lambda _: SomeActivation(),
+        # lambda _: nn.MaxPool2d(2, 2),  # 8
+        # lambda x: FixedPCA(next_cpt_weights(x, tn_repr, 3, 128, 128), 3),
+        # lambda _: nn.BatchNorm2d(256, affine=False, track_running_stats=False),
+        # lambda _: SomeActivation(),
+        # # lambda x: FixedPCA(next_lda_weights(x, tn_repr, 3, 256, 128), 3),
+        # # lambda _: nn.BatchNorm2d(256, affine=False, track_running_stats=False),
+        # # lambda _: SomeActivation(),
+        # lambda _: nn.MaxPool2d(2, 2),  # 4
+        # lambda x: FixedPCA(next_cpt_weights(x, tn_repr, 3, 256, 256), 3),
+        # lambda _: nn.BatchNorm2d(512, affine=False, track_running_stats=False),
+        # lambda _: SomeActivation(),
+        # lambda x: FixedPCA(next_lda_weights(x, tn_repr, 3, 512, 256), 3),
+        # lambda _: nn.BatchNorm2d(512, affine=False, track_running_stats=False),
+        # lambda _: SomeActivation(),
+        # lambda _: nn.MaxPool2d(2, 2),  # 2
+        # lambda x: FixedPCA(next_lda_weights(x, tn_repr, 3, 512, 512), 3),
+        # lambda _: SomeActivation(),
+        # lambda _: MinMaxPool(lambda: nn.AdaptiveMaxPool2d((1, 1))),  # 1
         lambda _: nn.Flatten(),
     ]
     encoder_layers = []
@@ -103,29 +181,33 @@ if __name__ == '__main__':
             layer = layer_config(encoded_x).to(tn_repr)
             encoder_layers.append(layer)
             encoded_x = [layer(x.to(tn_repr)) for x in encoded_x]
-    cla = nn.Sequential(nn.Linear(1024, 4096), nn.ReLU(), nn.Linear(4096, 10))
+    cla = nn.Sequential(nn.LazyLinear(10))
     model = nn.Sequential(*encoder_layers, cla).to(tn_repr)
     opt = torch.optim.Adam(cla.parameters())
     prog = tqdm.trange(50)
     test_x, test_y = list(get_test_cifar(10000))[0]
     t_acc = 0.0
     for epoch in prog:
+        model.train()
         for x, y in train:
             x, y = x.to(tn_repr), y.to(tn_repr.device)
             opt.zero_grad()
+            pred = model(x)
+            cls_loss = F.cross_entropy(pred, y)
             rob_loss = (cla[-1].weight.abs().sum(-1) ** 2).sum().sqrt()
-            cls_loss = F.cross_entropy(model(x), y)
-            (cls_loss + rob_loss * 3e-5).backward()
-            prog.set_description("C: %.4f, R: %.4f, T: %.4f" % (cls_loss, rob_loss, t_acc))
+            (cls_loss + rob_loss * 0.).backward()
+            acc = (pred.max(-1)[-1] == y).float().mean().item()
+            prog.set_description("C: %.3f, R: %.3f, A: %.3f, T: %.3f" % (cls_loss, rob_loss, acc, t_acc))
             opt.step()
         with torch.no_grad():
+            model.eval()
             t_acc = (
-                WrappedModel(model, 500)(test_x.to(tn_repr)).cpu().numpy().argmax(-1)
+                WrappedModel(model, 400)(test_x.to(tn_repr)).cpu().numpy().argmax(-1)
                 == test_y.numpy()
             ).mean()
     eval_model.eval_model_pgd(model, test, tn_repr.device, 4 / 255, 8 / 255, 20, 'cifar10')
     eval_model.eval_model_with_attack(
-        WrappedModel(model, 500), get_test_cifar(10000),
+        WrappedModel(model, 400), get_test_cifar(10000),
         EnergyAttack(4 / 255, 8 / 255, 10000),
         8 / 255, tn_repr.device, 'cifar10'
     )
